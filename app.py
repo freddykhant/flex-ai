4d4a1110754e9d48772dc63966a6067f7747dd10
Freddy Khant <freddyhmk@gmail.com>
2025-02-03 10:49:22 +0800

    config, title, and input box

--- app.py
+++ app.py
@@ -2,3 +2,12 @@ import streamlit as st
 import json
 from graph import graph, ChatState
 
+# streamlit page config
+st.set_page_config(page_title="Flex AI", page_icon="💪", layout="wide")
+
+# title and intro
+st.title("💪Flex AI: Your AI Personal Trainer")
+st.write("Ask me anything about hypertophy, resistance training, nutrition, and muscle growth!")
+
+# user input
+user_question = st.text_input("Enter your question:")
